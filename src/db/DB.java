7a9ff029df6281433eaf979c220fdfe0fd491c62
Franklyn <franklyn1998@live.com>
2021-10-17 11:41:19 -0300

    Padrão de projeto Observer para atualizar a TableView

--- src/db/DB.java
+++ src/db/DB.java
@@ -12,21 +12,20 @@ import java.util.Properties;
 public class DB {
 
 	private static Connection conn = null;
-	
+
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
 	}
-	
+
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
@@ -36,18 +35,17 @@ public class DB {
 			}
 		}
 	}
-	
+
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
 	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
