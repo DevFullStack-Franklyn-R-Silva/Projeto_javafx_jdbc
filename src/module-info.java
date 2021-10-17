b0fa372b823774ccc4a8c86dc0722bd79c5f2e18
Franklyn <franklyn1998@live.com>
2021-10-17 09:57:34 -0300

    Adicionando acesso ao banco de dados

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module Projeto_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
